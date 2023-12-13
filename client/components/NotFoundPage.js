d1a4f0d23f58c8ca483eb0683f00a27d9991610a
kristina <kristina@township.agency>
2023-12-13 18:22:29 -0500

    feat: navigation

---
+++ client/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <div className='nfp-container'>
+        <p className='nfp-info'>404 Not Found</p>
+      </div>
+    </>
+  );
+}
