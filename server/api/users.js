50e74a443a00263f14fa0b313aa68d386e891dbf
Kristina Stefanelli <kristinastefanelli@gmail.com>
2023-12-13 18:02:22 -0500

    feat: add api and db (#2)

---
+++ server/api/users.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const {
+  models: { User, Order, Product },
+} = require('../db');
+const { requireToken, isAdmin } = require('./middleware');
+module.exports = router;
+
+router.get('/', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        isAdmin: false
+      }
+    });
+    res.send(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    const userToken = await newUser.generateToken();
+    res.send(userToken);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/profile', requireToken, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.dataValues.id, {
+      attributes: ['id', 'firstName', 'lastName', 'email', 'password', 'address'],
+    });
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/account', requireToken, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.dataValues.id, {
+      attributes: ['id', 'firstName', 'lastName', 'password', 'email', 'address'],
+    });
+    await user.update(req.body);
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/orders', requireToken, async (req, res, next) => {
+  try {
+    const userOrder = await Order.findAll({
+      where: {
+        userId: req.user.dataValues.id,
+        status: 'closed',
+      },
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    res.send(userOrder);
+  } catch (err) {
+    next(err);
+  }
+});
---
+++ server/db/models/User.js
@@ -0,0 +1,96 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 5;
+
+const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notEmpty: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = User;
+
+
+User.prototype.correctPassword = function (candidatePwd) {
+  return bcrypt.compare(candidatePwd, this.password);
+};
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
+
+
+User.authenticate = async function ({ email, password }) {
+  const user = await this.findOne({ where: { email } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password');
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
+};
+
+User.findByToken = async function (token) {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
+    if (!user) {
+      throw 'nooo';
+    }
+    return user;
+  } catch (ex) {
+    const error = Error('bad token');
+    error.status = 401;
+    throw error;
+  }
+};
+
+
+const hashPassword = async (user) => {
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+  }
+};
+
+User.beforeCreate(hashPassword);
+User.beforeUpdate(hashPassword);
+User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
+
+module.exports = User;
