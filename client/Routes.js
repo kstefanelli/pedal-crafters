76b761bb89b4caec2477fabe76c02fdb2ba075ce
Kristina Stefanelli <kristinastefanelli@gmail.com>
2023-12-13 18:51:44 -0500

    feat: about page (#6)

--- client/Routes.js
+++ client/Routes.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import { me } from './store';
-import NotFoundPage from './components/NotFoundPage';
-import Cart from './components/Cart';
-import Products from './components/Products';
-import SingleProduct from './components/SingleProduct';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import { me } from "./store";
+import NotFoundPage from "./components/NotFoundPage";
+import Cart from "./components/Cart";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
+import About from "./components/About";
 
 class Routes extends Component {
   componentDidMount() {
@@ -24,29 +24,32 @@ class Routes extends Component {
             {isAdmin ? (
               <Switch>
                 {/* Routes if logged in and admin */}
-                <Route exact path="/" component={Home} />
-                <Route path="/products/:id" component={SingleProduct} />
-                <Route exact path="/cart" component={Cart} />
-                <Route path="*" component={NotFoundPage} status={404} />
+                <Route exact path='/' component={Home} />
+                <Route path='/products/:id' component={SingleProduct} />
+                <Route exact path='/cart' component={Cart} />
+                <Route path='/about' component={About} />
+                <Route path='*' component={NotFoundPage} status={404} />
               </Switch>
             ) : (
               <Switch>
                 {/* Routes if logged in but not admin */}
-                <Route exact path="/" component={Home} />
-                <Route exact path="/products" component={Products} />
-                <Route path="/products/:id" component={SingleProduct} />
-                <Route exact path="/cart" component={Cart} />
-                <Route path="*" component={NotFoundPage} status={404} />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/products' component={Products} />
+                <Route path='/products/:id' component={SingleProduct} />
+                <Route exact path='/cart' component={Cart} />
+                <Route path='/about' component={About} />
+                <Route path='*' component={NotFoundPage} status={404} />
               </Switch>
             )}
           </div>
         ) : (
           <Switch>
             {/* Routes if not logged in */}
-            <Route exact path="/" component={Home} />
-            <Route path="/products/:id" component={SingleProduct} />
-                <Route exact path="/cart" component={Cart} />
-            <Route path="*" component={NotFoundPage} status={404} />
+            <Route exact path='/' component={Home} />
+            <Route path='/products/:id' component={SingleProduct} />
+            <Route exact path='/cart' component={Cart} />
+            <Route path='/about' component={About} />
+            <Route path='*' component={NotFoundPage} status={404} />
           </Switch>
         )}
       </div>
