42cdd50a22b17fd76db681655979730abdd6318f
kristina <kristina@township.agency>
2023-12-13 19:22:24 -0500

    feat: users

---
+++ client/components/OrderHistory.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchOrders } from '../store/allOrders';
+
+const OrderHistory = ({ orders, getOrders }) => {
+  useEffect(() => {
+    getOrders();
+  }, [getOrders]);
+
+  return (
+    <div className="order-section">
+      <h3 style={{ marginLeft: '2rem' }}>Order History</h3>
+      {orders && orders.length !== 0 ? (
+        <div className="all-order-container">
+          {orders.map((order) => (
+            <div order={order} key={order.id} className="order-container">
+              <h4 style={{ marginLeft: '2rem' }}>Order number: {order.id}</h4>
+              <div className="order-item-container">
+                <div></div>
+                <div style={{ fontWeight: 'bold' }} className="order-name">
+                  Name
+                </div>
+                <div style={{ fontWeight: 'bold' }} className="order-price">
+                  Price
+                </div>
+                <div style={{ fontWeight: 'bold' }} className="order-quantity">
+                  Quantity
+                </div>
+                <div style={{ fontWeight: 'bold' }}>Total</div>
+              </div>
+              {order.products.map((item) => (
+                <div key={item.id} className="order-item-container">
+                  <img className="order-img" src={item.imageURL} alt={`Image of ${item.name}`} />
+                  <div className="s-order-name">
+                    <Link to={`/products/${item.id}`}>
+                      <span>{item.name}</span>
+                    </Link>
+                  </div>
+                  <span style={{ fontSize: '.8rem' }} className="s-order-price">
+                    {(item.price / 10000).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+                  </span>
+                  <span style={{ fontSize: '.8rem' }} className="s-order-quantity">
+                    {item.cartItem.quantity}
+                  </span>
+                  <span style={{ fontSize: '.8rem' }}>
+                  {((item.cartItem.quantity * item.price) / 10000).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+                  </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="div-container">No recent orders found</div>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  orders: state.orders,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getOrders: () => dispatch(fetchOrders()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderHistory);
---
+++ client/components/UserProfile.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../store/singleUser';
+import { logout } from '../store';
+import OrderHistory from './OrderHistory';
+import { Link } from 'react-router-dom';
+
+export class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getUser();
+  }
+
+  render() {
+    return (
+      <div className="profile-container">
+        <h2 className="profile-heading">Welcome {this.props.firstName}!</h2>
+        <div className="profile-btn-container">
+          <Link to="/profile/update">
+            <button type="button" className="update-profile">
+              Update profile
+            </button>
+          </Link>
+          <button
+            type="button"
+            className="update-profile"
+            onClick={this.props.handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+        <div className="profile-order-container">
+          <OrderHistory />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  allOrders: state.orders,
+  firstName: state.auth.firstName,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getUser: () => dispatch(fetchUser()),
+  handleLogout: () => dispatch(logout()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
