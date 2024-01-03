77c3b7cfb0ce1d00f607a2f0b0d4f94884efc64a
Kristina Stefanelli <kristinastefanelli@gmail.com>
2024-01-03 17:34:08 -0500

    feat: auth updates (#16)

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,71 +1,80 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { authenticate } from '../store';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { authenticate } from "../store";
 
-/**
- * COMPONENT
- */
-const AuthForm = (props) => {
-  const { name, displayName, handleSubmit, error } = props;
-  console.log(props);
+const Input = ({ type, name, placeholder, required }) => (
+  <input
+    className='w-full p-2 mb-4 border rounded'
+    type={type}
+    name={name}
+    placeholder={placeholder}
+    required={required}
+  />
+);
+
+const Button = ({ type, label, className }) => (
+  <button className={`w-full ${className}`} type={type}>
+    {label}
+  </button>
+);
+
+const AuthForm = ({ name, displayName, handleSubmit, error }) => {
   return (
-    <div className="form div-container ">
+    <div className='form div-container '>
       <form onSubmit={handleSubmit} name={name}>
-        <div className="login">
-          <div className="login-card">
-            <p className="title">Sign in</p>
-            <input name="email" placeholder="Email" required />
-            <input
-              name="password"
-              placeholder="Password"
-              type="password"
+        <div className='login'>
+          <div className='login-card'>
+            <p className='title'>Sign in</p>
+            <Input name='email' placeholder='Email' required />
+            <Input
+              name='password'
+              placeholder='Password'
+              type='password'
               required
             />
-            <button className="loader">Sign in</button>
-            <p className="text">Don't have an account?</p>
-            <Link to="/register">
-              <button className="buttonShadow" type="submit">
-                Create new account
-              </button>
+            <Button
+              type='submit'
+              label='Sign in'
+              className='bg-[#085162] text-white p-2 rounded hover:opacity-50'
+            />
+            <p className='text'>Don't have an account?</p>
+            <Link to='/register'>
+              <Button
+                type='submit'
+                label='Create new account'
+                className='bg-[#085162] text-white p-2 rounded hover:opacity-50'
+              />
             </Link>
           </div>
         </div>
-        {error && <div> {error} </div>}
+        {error && <div className='text-red-500'> {error} </div>}
       </form>
     </div>
   );
 };
 
-const mapSignIn = (state) => {
-  return {
-    name: 'signIn',
-    displayName: 'SignIn',
+const mapSignIn = (state) => ({
+  name: "signIn",
+  displayName: "SignIn",
+  error: state.auth.error,
+});
 
-    error: state.auth.error,
-  };
-};
+const mapRegister = (state) => ({
+  name: "register",
+  displayName: "Register",
+  error: state.auth.error,
+});
 
-const mapRegister = (state) => {
-  return {
-    name: 'register',
-    displayName: 'Register',
-    error: state.auth.error,
-  };
-};
-
-const mapDispatch = (dispatch) => {
-  return {
-    handleSubmit(evt) {
-      evt.preventDefault();
-      const formName = evt.target.name;
-      const email = evt.target.email.value;
-      const password = evt.target.password.value;
-      dispatch(authenticate(email, password, formName));
-    },
-  };
-};
+const mapDispatch = (dispatch) => ({
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const formName = evt.target.name;
+    const email = evt.target.email.value;
+    const password = evt.target.password.value;
+    dispatch(authenticate(email, password, formName));
+  },
+});
 
 export const SignIn = connect(mapSignIn, mapDispatch)(AuthForm);
-
 export const Register = connect(mapRegister, mapDispatch)(AuthForm);
