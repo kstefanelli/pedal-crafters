a0ce16dbad172c630a827de6e92d01496bc92b00
kristina <kristina@township.agency>
2024-01-04 15:52:59 -0500

    feat: update users

--- client/components/AdminUsers.js
+++ client/components/AdminUsers.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchUsers } from '../store/allUsers';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../store/allUsers";
 
-export class AdminUsers extends Component {
-  async componentDidMount() {
-    await this.props.getAllUsers();
-  }
+const UserTableItem = ({ user, isHeader }) => (
+  <tr className={isHeader ? "all-users-content-label" : "all-users-content"}>
+    <td className={isHeader ? "table-label-name" : "table-content-name"}>
+      {isHeader ? "First name" : user.firstName}
+    </td>
+    <td className={isHeader ? "table-label-name" : "table-content-name"}>
+      {isHeader ? "Last name" : user.lastName}
+    </td>
+    <td className={isHeader ? "table-label-long" : "table-content-long"}>
+      {isHeader ? "Email" : user.email}
+    </td>
+    <td className={isHeader ? "table-label-long" : "table-content-long"}>
+      {isHeader ? "Address" : user.address}
+    </td>
+  </tr>
+);
 
-  render() {
-    const users = this.props.users;
-    return (
-      <div className="all-users-container ">
-        <h2>Customer Information</h2>
-        <table className="all-users-table">
-          <tbody className="all-users-table-body">
-            <tr className="all-users-content-label">
-              <td className="table-label-name">First name</td>
-              <td className="table-label-name">Last name</td>
-              <td className="table-label-long">Email</td>
-              <td className="table-label-long">Address</td>
-            </tr>
-            {users.map((user) => (
-              <tr key={user.id} className="all-users-content">
-                <td className="table-content-name">{user.firstName}</td>
-                <td className="table-content-name">{user.lastName} </td>
-                <td className="table-content-long">{user.email} </td>
-                <td className="table-content-long">{user.address} </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+const AdminUsers = ({ users, getAllUsers }) => {
+  useEffect(() => {
+    getAllUsers();
+  }, [getAllUsers]);
+
+  return (
+    <div className='all-users-container'>
+      <h2 className='pt-5'>Customer Information</h2>
+      <table className='all-users-table'>
+        <tbody className='all-users-table-body'>
+          <UserTableItem isHeader={true} />
+          {users.map((user) => (
+            <UserTableItem key={user.id} user={user} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   users: state.users,
