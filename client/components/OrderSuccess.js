b89798a057c6659122fb2cf17a5a0667e3df8e57
kristina <kristina@township.agency>
2024-01-05 08:30:27 -0500

    feat: update orders

--- client/components/OrderSuccess.js
+++ client/components/OrderSuccess.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { emptyCart } from "../store/cart";
-class OrderSuccess extends React.Component {
-  componentDidMount() {
-    this.props.emptyCart(this.props.cart);
-  }
 
-  render() {
-    return (
-      <div className='div-container '>
-        <h1 style={{ textAlign: "center", marginTop: "150px" }}>
-          Thank you for your order!
-        </h1>
-        <h3 style={{ textAlign: "center" }}>
-          Your Order Number: {Math.floor(Math.random() * Math.floor(999))}-
-          {Math.floor(Math.random() * Math.floor(999))}-
-          {Math.floor(Math.random() * Math.floor(9999))}
-        </h3>
-      </div>
-    );
-  }
-}
+const OrderSuccess = ({ cart, emptyCart }) => {
+  useEffect(() => {
+    const emptyUserCart = async () => {
+      try {
+        await emptyCart(cart);
+      } catch (error) {
+        console.error("Error emptying cart:", error);
+      }
+    };
 
-const mapStateToProps = (state) => {
-  return {
-    cart: state.cart,
+    emptyUserCart();
+  }, [cart, emptyCart]);
+
+  const generateOrderNumber = () => {
+    const getRandomNumber = () => Math.floor(Math.random() * 9999);
+    return `${getRandomNumber()}-${getRandomNumber()}-${getRandomNumber()}`;
   };
+
+  return (
+    <div className='div-container'>
+      <h1 style={{ textAlign: "center", marginTop: "150px" }}>
+        Thank you for your order!
+      </h1>
+      <h3 style={{ textAlign: "center" }}>
+        Your Order Number: {generateOrderNumber()}
+      </h3>
+    </div>
+  );
 };
+
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   emptyCart: (cart) => dispatch(emptyCart(cart)),
