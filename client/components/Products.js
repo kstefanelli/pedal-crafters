ae348087b75d3c55290a437efe09112889aba8e4
kristina <kristina@township.agency>
2024-01-08 10:37:22 -0500

    feat: fix product card

--- client/components/Products.js
+++ client/components/Products.js
@@ -38,7 +38,7 @@ const Products = ({ products, getProducts, addToCart }) => {
           />
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8'>
           {productFilter.map((product) =>
             product.name.toLowerCase().includes(term.toLowerCase()) ? (
               <ProductItem
