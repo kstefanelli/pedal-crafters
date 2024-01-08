e761591a90adcc6e3c2b854b12d42dc9c064221b
Kristina Stefanelli <kristinastefanelli@gmail.com>
2024-01-08 10:37:53 -0500

    feat: fix product card (#20)

--- client/components/ProductItem.js
+++ client/components/ProductItem.js
@@ -2,34 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProductItem = ({ product, addToCart, isAdminView }) => (
-  <div
-    className={`group flex flex-col hover:cursor-pointer items-center justify-between gap-4 px-4 py-6 rounded-lg bg-white border border-ocean relative overflow-hidden transition-shadow duration-300 ease-in-out transform hover:shadow-lg`}
-  >
-    <Link to={`/products/${product.id}`}>
+  <div className='group p-4 cards z-10 text-center shadow-lg bg-white flex flex-col gap-4 custom-drop rounded-xl overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105'>
+    <Link to={`/products/${product.id}`} className='flex flex-col grow'>
       <img
-        className='mx-auto aspect-auto h-auto w-auto'
+        className='aspect-video object-cover overflow-hidden'
         src={product.imageURL}
         alt={`Image of ${product.name}`}
       />
     </Link>
-    <h3 className='text-center text-sm'>{product.name}</h3>
-    <div className='text-center text-sm'>{`$${parseFloat(
-      product.price / 100
-    ).toFixed(2)}`}</div>
-    <div className='flex flex-col space-y-2 justify-center'>
-      {isAdminView && (
-        <Link to={`/products/${product.id}/update`}>
-          <button className='bg-[#085162] hover:opacity-50 text-white text-sm font-bold py-2 px-4 rounded'>
-            Edit Product
-          </button>
-        </Link>
-      )}
-      <button
-        className='bg-[#085162] hover:opacity-50 text-white text-sm font-bold py-2 px-4 rounded'
-        onClick={() => addToCart(product)}
-      >
-        Add To Cart
-      </button>
+
+    <div className='flex flex-col gap-2'>
+      <Link to={`/products/${product.id}`} className='flex flex-col grow'>
+        <b>{product.name}</b>
+      </Link>
+
+      <div>{`$${parseFloat(product.price / 100).toFixed(2)}`}</div>
+    </div>
+    <div className='flex justify-center cursor-pointer'>
+      <div className='flex flex-col gap-2'>
+        {isAdminView && (
+          <Link to={`/products/${product.id}/update`}>
+            <button className='bg-[#085162] hover:opacity-50 text-white text-sm font-bold py-2 px-4 rounded'>
+              Edit Product
+            </button>
+          </Link>
+        )}
+        <button
+          className='bg-[#085162] hover:opacity-50 text-white text-sm font-bold py-2 px-4 rounded'
+          onClick={() => addToCart(product)}
+        >
+          Add To Cart
+        </button>
+      </div>
     </div>
   </div>
 );
