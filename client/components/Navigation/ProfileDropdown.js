8f6f5fe44829040098acfece9eac31bfedfaea87
Kristina Stefanelli <kristinastefanelli@gmail.com>
2024-02-06 16:14:45 -0500

    feat: update mobile dropdown (#49)

--- client/components/Navigation/ProfileDropdown.js
+++ client/components/Navigation/ProfileDropdown.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchUser } from "../../store/singleUser";
 import { logout } from "../../store";
+
 const ProfileDropdown = ({ handleLogout, isAdmin }) => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
@@ -30,59 +31,106 @@ const ProfileDropdown = ({ handleLogout, isAdmin }) => {
     { to: "/products/add", text: "ADD PRODUCTS" },
   ];
 
+  const dropdownStyles =
+    "bg-[#f4f1e0] rounded-md shadow-lg py-2 mt-1 flex flex-col z-10 lg:right-12 lg:min-w-[150px]";
+  const buttonStyles =
+    "font-bold hover:text-[#FFA364] transition duration-500 hover:underline hover:decoration-2 text-2xl lg:text-xl relative z-10 m-0";
+  const linkStyles =
+    "px-4 py-2 font-bold text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:decoration-2";
+
   return (
     <div
       className='relative inline-block'
       onMouseEnter={toggleDropdown}
       onMouseLeave={hideDropdown}
     >
-      <button
-        className='font-bold hover:text-[#FFA364] transition duration-500 hover:underline hover:underline-offset-4 hover:decoration-2 text-2xl lg:text-xl'
-        style={{ margin: 0, position: "relative", zIndex: 10 }}
-        onClick={toggleDropdown}
-      >
+      <button className={buttonStyles} onClick={toggleDropdown}>
         ACCOUNT
       </button>
-      <div
-        className={`${
-          isDropdownVisible ? "block" : "hidden"
-        } absolute bg-[#f4f1e0] rounded-md shadow-lg py-2 mt-1 flex flex-col text-start`}
-        style={{ right: 75, position: "absolute", zIndex: 10 }}
-      >
-        {dropdownLinks.map((link, index) => (
-          <Link
-            key={index}
-            to={link.to}
-            onClick={handleDropdownItemClick}
-            className={`px-4 py-2 font-bold text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:underline-offset-4 hover:decoration-2`}
-          >
-            {link.text}
-          </Link>
-        ))}
-        {isAdmin && (
-          <>
-            {adminLinks.map((link, index) => (
-              <Link
-                key={index}
-                to={link.to}
-                onClick={handleDropdownItemClick}
-                className={`px-4 py-2 font-bold text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:underline-offset-4 hover:decoration-2`}
-              >
-                {link.text}
-              </Link>
-            ))}
-          </>
-        )}
-        <button
-          type='button'
-          onClick={() => {
-            handleLogout();
-            handleDropdownItemClick();
-          }}
-          className={`text-start font-bold px-4 py-2 text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:underline-offset-4 hover:decoration-2`}
+      <div className='hidden lg:block'>
+        <div
+          className={`${
+            isDropdownVisible ? "block" : "hidden"
+          } ${dropdownStyles} text-start`}
+          style={{ position: "absolute" }}
         >
-          LOG OUT
-        </button>
+          {dropdownLinks.map((link, index) => (
+            <Link
+              key={index}
+              to={link.to}
+              onClick={handleDropdownItemClick}
+              className={linkStyles}
+            >
+              {link.text}
+            </Link>
+          ))}
+          {isAdmin && (
+            <>
+              {adminLinks.map((link, index) => (
+                <Link
+                  key={index}
+                  to={link.to}
+                  onClick={handleDropdownItemClick}
+                  className={linkStyles}
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </>
+          )}
+          <button
+            type='button'
+            onClick={() => {
+              handleLogout();
+              handleDropdownItemClick();
+            }}
+            className='text-start font-bold px-4 py-2 text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:decoration-2'
+          >
+            LOG OUT
+          </button>
+        </div>
+      </div>
+      <div className='lg:hidden'>
+        <div
+          className={`${
+            isDropdownVisible ? "block" : "hidden"
+          } ${dropdownStyles}`}
+        >
+          {dropdownLinks.map((link, index) => (
+            <Link
+              key={index}
+              to={link.to}
+              onClick={handleDropdownItemClick}
+              className={linkStyles}
+            >
+              {link.text}
+            </Link>
+          ))}
+          {isAdmin && (
+            <>
+              {adminLinks.map((link, index) => (
+                <Link
+                  key={index}
+                  to={link.to}
+                  onClick={handleDropdownItemClick}
+                  className={linkStyles}
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </>
+          )}
+          <button
+            type='button'
+            onClick={() => {
+              handleLogout();
+              handleDropdownItemClick();
+            }}
+            className='font-bold px-4 py-2 text-[#321E1E] text-lg hover:text-[#FFA364] transition duration-500 hover:underline hover:decoration-2'
+          >
+            LOG OUT
+          </button>
+        </div>
       </div>
     </div>
   );
